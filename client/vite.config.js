ac1cf70627d50a27dd2c1037c07b0b5d7b4d727b
malhotrasarika789-blip <malhotrasarika789@gmail.com>
2026-08-21 10:02:04 +0530

    Fix Vite configuration for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import path from "path";
 
 export default defineConfig({
     plugins: [react()],
+
+    css: {
+        transformer: "postcss",
+    },
+
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
