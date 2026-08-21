a2369118cfe4529b64c995a17512d854e2a28fc7
malhotrasarika789-blip <malhotrasarika789@gmail.com>
2026-08-21 10:07:48 +0530

    Connect frontend with deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-    import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
+const API_URL = "https://shopsphere-8v7i.onrender.com/api";
 const api = axios.create({
     baseURL: API_URL,
     headers: {
